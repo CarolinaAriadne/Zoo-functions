4673eb5fbe5f004bee2cad504216592f2f3cc1d1
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-11-24 22:06:47 -0300

    Requisito-1-com-comentario

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,11 @@
 const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  const especiesFiltradas = species.filter((especie) => ids.includes(especie.id));
+function getSpeciesByIds(...ids) { // ids que estão no teste
+  const especiesFiltradas = species.filter((especie) => ids.includes(especie.id)); // se existir no elemento da iteração (especie), se inclui, um id que seja igual a ids (os que) estão colocados em teste - linha 9
 
-  if (especiesFiltradas.length === 0) {
+  if (especiesFiltradas.length === 0) { // se especiesFiltradas, verifica que não existe nas iterações nos elementos, o id igual ao(s) id(s) do parâmetro, retorna para a função, um array vazio
     return [];
   }
-  return especiesFiltradas;
+  return especiesFiltradas; // se sim, retorno especiesFiltradas (um array com meu objeto que satisfaz a condição) para a função
 }
 module.exports = getSpeciesByIds;
